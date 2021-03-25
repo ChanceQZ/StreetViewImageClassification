5e68bafa32c36a709fdaa14c80bde536c7ed71bc
zhenqian <qz_gis@163.com>
2021-03-25 14:33:22 +0800

    fix sampling method

--- sampling.py
+++ sampling.py
@@ -7,6 +7,7 @@
 @Date: 3/23/2021
 """
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import StratifiedShuffleSplit as SSS
 
 
@@ -30,18 +31,31 @@ class DataSplit:
         :param test_sampling_size: Size of test sampling dataset, 0<i<1
         :param sampling_times_size: Sampling times and size of validation and test set
         """
-        self.sampling_set_split(validation_sampling_size, test_sampling_size)
+        self._training_sampling_idx = None
+        self._union_test_sampling_idx = None
+        self._validation_sampling_idx = None
+        self._test_sampling_idx = None
+
+        self.sampling_set_split(X, y, validation_sampling_size, test_sampling_size)
+
+        self.training_sampling = X[self._training_sampling_idx]
+        self.validation_sampling = X[self._union_test_sampling_idx][self._validation_sampling_idx]
+        self.test_sampling = X[self._union_test_sampling_idx][self._test_sampling_idx]
+
+        self.training_sampling_label = y[self._training_sampling_idx]
+        self.validation_sampling_label = y[self._union_test_sampling_idx][self._validation_sampling_idx]
+        self.test_sampling_label = y[self._union_test_sampling_idx][self._test_sampling_idx]
 
         self.validation_sampling_list = self.repeat_sampling(
-            X[self._validation_sampling_idx],
-            y[self._validation_sampling_idx],
+            self.validation_sampling,
+            self.validation_sampling_label,
             self.split,
             *sampling_times_size
         )
 
         self.test_sampling_list = self.repeat_sampling(
-            X[self._test_sampling_idx],
-            y[self._test_sampling_idx],
+            self.test_sampling,
+            self.test_sampling_label,
             self.split,
             *sampling_times_size
         )
@@ -75,7 +89,7 @@ class DataSplit:
         )
         return next(sss.split(X, y))
 
-    def sampling_set_split(self, validation_sampling_size, test_sampling_size):
+    def sampling_set_split(self, X, y, validation_sampling_size, test_sampling_size):
         self._training_sampling_idx, self._union_test_sampling_idx = self.split(
             X, y,
             validation_sampling_size + test_sampling_size
@@ -88,9 +102,36 @@ class DataSplit:
         )
 
 
-if __name__ == "__main__":
-    X = np.random.randint(0, 10, (10000, 4))
-    y = np.random.randint(0, 2, 10000)
+def main(X, y):
     ds = DataSplit(X, y)
-    print(len(ds.validation_sampling_list))
-    print(len(ds.test_sampling_list))
+
+    # writing training sampling data
+    with open("./data/training_sampling.txt", "w") as f:
+        for train in ds.training_sampling:
+            f.write(train)
+            f.write("\n")
+
+    # writing validation data
+    for idx, validation_set in enumerate(ds.validation_sampling_list):
+        with open("./data/validation_set/validation_%d.txt" % idx, "w") as f:
+            for validation_img in validation_set:
+                f.write(validation_img)
+                f.write("\n")
+
+    # writing test data
+    for idx, test_set in enumerate(ds.test_sampling_list):
+        with open("./data/test_set/test_%d.txt" % idx, "w") as f:
+            for test_img in test_set:
+                f.write(test_img)
+                f.write("\n")
+
+
+if __name__ == "__main__":
+    city_label = pd.read_csv("./data/city_label.csv")
+    image_city = pd.read_csv("./data/image_city.csv")
+    image_label = pd.merge(city_label, image_city, on="name")
+    # image_label[["name", "label", "img_path"]].to_csv("image_label.csv", index=None)
+    X = image_label["img_path"].values
+    y = image_label["label"].values
+
+    main(X, y)
